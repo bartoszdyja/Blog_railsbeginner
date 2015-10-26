9f8e8a9fb2a7f8b5125de7209f37d31071bc8f75
railsbeginner <bartosz.dyja@gmail.com>
2015-10-26 21:30:11 +0100

    Add slim gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
+  root to: 'blogs#index'
+  resources :blogs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
