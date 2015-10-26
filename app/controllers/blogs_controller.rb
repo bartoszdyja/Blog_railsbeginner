151a6b21afbbb5a3d59035168ade9b706dd617c5
railsbeginner <bartosz.dyja@gmail.com>
2015-10-26 22:51:37 +0100

    Add blog views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,19 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to root_path,  notice: 'Successfully created'
+  end
+
   def index
+    @blogs = Blog.all
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
